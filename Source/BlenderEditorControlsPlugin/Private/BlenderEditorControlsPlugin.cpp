eec2aa8c9b24a90a9ca3f9d3e0888e0e8f5482fd
jefimh <100091347+jefimh@users.noreply.github.com>
2025-06-17 16:55:37 +0200

    Create folder structure

--- Source/BlenderEditorControlsPlugin/Private/BlenderEditorControlsPlugin.cpp
+++ Source/BlenderEditorControlsPlugin/Private/BlenderEditorControlsPlugin.cpp
@@ -1,20 +1,32 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "BlenderEditorControlsPlugin.h"
+#include "Modules/ModuleManager.h"
+#include "Logging/LogMacros.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogBlenderEditorControls, Log, All);
 
 #define LOCTEXT_NAMESPACE "FBlenderEditorControlsPluginModule"
 
 void FBlenderEditorControlsPluginModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+    UE_LOG(LogBlenderEditorControls, Log, TEXT("BlenderEditorControlsPlugin: StartupModule"));
+
+    // GrabProcessor = MakeShared<FGrabInputProcessor>();
+    // FSlateApplication::Get().RegisterInputPreProcessor(GrabProcessor);
 }
 
 void FBlenderEditorControlsPluginModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+    UE_LOG(LogBlenderEditorControls, Log, TEXT("BlenderEditorControlsPlugin: ShutdownModule"));
+    /*if (FSlateApplication::IsInitialized())
+    {
+        FSlateApplication::Get().UnregisterInputPreProcessor(GrabProcessor);
+    }
+    GrabProcessor.Reset();
+    */
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+
 IMPLEMENT_MODULE(FBlenderEditorControlsPluginModule, BlenderEditorControlsPlugin)
