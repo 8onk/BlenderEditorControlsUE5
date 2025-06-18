6a6d6510e2d0471d1cb91bf884c35117b37b273f
jefimh <100091347+jefimh@users.noreply.github.com>
2025-06-18 13:40:59 +0200

    Implement BeginTool

--- Source/BlenderEditorControlsPlugin/Private/Input/BlenderEditorControlsPluginInputProcessor.cpp
+++ Source/BlenderEditorControlsPlugin/Private/Input/BlenderEditorControlsPluginInputProcessor.cpp
@@ -1,1 +1,85 @@
- 
+#include "Input/BlenderEditorControlsPluginInputProcessor.h"
+#include "Tools/BlenderToolBase.h"
+#include "Tools/MoveTool.h"
+#include "Tools/RotateTool.h"
+#include "Tools/ScaleTool.h"
+
+namespace BlenderControls
+{
+    FBlenderControlsInputProcessor::FBlenderControlsInputProcessor(TSharedPtr<FUICommandList> InCommandList) : CommandList(InCommandList)
+    {
+    }
+
+    FBlenderControlsInputProcessor::~FBlenderControlsInputProcessor()
+    {
+    }
+
+    void FBlenderControlsInputProcessor::Tick(const float DeltaTime, FSlateApplication &, TSharedRef<ICursor>)
+    {
+    }
+
+    bool FBlenderControlsInputProcessor::HandleKeyDownEvent(FSlateApplication &, const FKeyEvent &)
+    {
+        return false;
+    }
+
+    bool FBlenderControlsInputProcessor::HandleKeyUpEvent(FSlateApplication &, const FKeyEvent &)
+    {
+        return false;
+    }
+
+    bool FBlenderControlsInputProcessor::HandleMouseMoveEvent(FSlateApplication &, const FPointerEvent &)
+    {
+        return false;
+    }
+
+    bool FBlenderControlsInputProcessor::HandleMouseButtonDownEvent(FSlateApplication &, const FPointerEvent &)
+    {
+        return false;
+    }
+
+    bool FBlenderControlsInputProcessor::HandleMouseButtonUpEvent(FSlateApplication &, const FPointerEvent &)
+    {
+        return false;
+    }
+
+    void FBlenderControlsInputProcessor::BeginTool(ETransformMode Mode)
+    {
+        if (CurrentTool.IsValid())
+        {
+            return;
+        }
+
+        const ETransformAxis InitialAxis = ETransformAxis::All;
+
+        switch (Mode)
+        {
+        case ETransformMode::Translate:
+            CurrentTool = MakeShared<FMoveTool>(InitialAxis);
+            break;
+        case ETransformMode::Rotate:
+            CurrentTool = MakeShared<FRotateTool>(InitialAxis);
+            break;
+        case ETransformMode::Scale:
+            CurrentTool = MakeShared<FScaleTool>(InitialAxis);
+            break;
+        default:
+            return;
+        }
+
+        ActiveMode = Mode;
+        bNumericInput = false;
+        NumericBuffer.Reset();
+
+        // Tell overlay to start drawing guides for this tool
+        if (Overlay.IsValid())
+        {
+            Overlay->SetContext(CurrentTool);
+        }
+    }
+
+    void FBlenderControlsInputProcessor::EndTool(bool bApply)
+    {
+    }
+
+} // namespace BlenderControls
