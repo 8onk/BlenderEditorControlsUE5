ae755a4df4458e9847a7b9d911e27481f1370d63
jefimh <100091347+jefimh@users.noreply.github.com>
2025-06-21 15:37:01 +0200

    Delete transform gizmo files. Note, this was when i tried to inherit UTransformGizmo.cpp to be able to call protected classes

--- Source/BlenderEditorControlsPlugin/Private/BEC_TransformGizmoBuilder.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "BEC_TransformGizmoBuilder.h"
-
-UInteractiveGizmo* UBEC_TransformGizmoBuilder::BuildGizmo(const FToolBuilderState& State) const
-{
-	UBEC_TransformGizmo* NewGizmo = NewObject<UBEC_TransformGizmo>(State.World);
-	NewGizmo->Initialize(State.World);  
-	return NewGizmo;
-}
