d7db3bf6b0f8e1aeea547932ce8a4c32d0bfe968
jefimh <100091347+jefimh@users.noreply.github.com>
2026-01-06 17:37:13 +0100

    Remove redundant comment

--- Source/BlenderEditorControlsPlugin/Private/TransformSession.cpp
+++ Source/BlenderEditorControlsPlugin/Private/TransformSession.cpp
@@ -366,7 +366,6 @@ namespace BlenderControls
 		// Add a sensitivity multiplier for ue 5.7, because ue 5.7 uses raw input, as mouse acceleration
 		// doesn't kick in for some reason in ue 5.7...
 #if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION >= 7
-		// Create a local sensitivity variable
 		FVector2D RawDelta = CurrentViewportMousePosition - CursorAnchorPoint;
 		CurrentViewportMousePosition = CursorAnchorPoint + (RawDelta * GetDefault<UBlenderControlsSettings>()->
 			MouseSensitivity);
