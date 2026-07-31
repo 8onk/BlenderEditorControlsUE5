a148f9d519eb3befcf05f3cbf40ecce4b3fb9618
jefimh <100091347+jefimh@users.noreply.github.com>
2026-08-01 00:33:10 +0200

    Remove comment

--- Source/BlenderEditorControlsPlugin/BlenderEditorControlsPlugin.Build.cs
+++ Source/BlenderEditorControlsPlugin/BlenderEditorControlsPlugin.Build.cs
@@ -76,7 +76,7 @@ public class BlenderEditorControlsPlugin : ModuleRules
 		);
 		
 		
-        if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion < 6) // for 5.5 compatibility, idk maybe lower would work too
+        if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion < 6)
         {
             PrivateDependencyModuleNames.Add("SequencerWidgets");
         }
